76d62ce730177034f227aa5b60b9cbacbde4efa1
Denis Sobolevsky <desobolevsky@gmail.com>
2021-07-19 22:40:29 +0300

    fix async function

--- sort/sortings.js
+++ sort/sortings.js
@@ -1,6 +1,7 @@
+// TODO: add possible arguments
 async function bubbleSort(array, colors){
-    for (i = 0; i < array.length - 1; ++i){
-        for (j = 0; j < array.length - i - 1; ++j){
+    for (let i = 0; i < array.length - 1; ++i){
+        for (let j = 0; j < array.length - i - 1; ++j){
             colors[j] = iteratedColor;
             chart.update();
             await sleep(currentSleepTime)
