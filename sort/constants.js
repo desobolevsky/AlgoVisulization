24d1008da67b2b498e03195b846da9b378450d5b
Denis Sobolevsky <desobolevsky@gmail.com>
2021-08-07 11:19:35 +0300

    create constants.js, add reset button

---
+++ sort/constants.js
@@ -0,0 +1,2 @@
+const DEFAULT_COLOR = 'rgb(135,135,135)';
+const ITERATED_COLOR = 'rgb(255, 0, 0)';
--- sort/sortings.js
+++ sort/sortings.js
@@ -2,23 +2,23 @@
 async function bubbleSort(array, colors){
     for (let i = 0; i < array.length - 1; ++i){
         for (let j = 0; j < array.length - i - 1; ++j){
-            colors[j] = iteratedColor;
+            colors[j] = ITERATED_COLOR;
             chart.update();
             await sleep(currentSleepTime)
             if (array[j] > array[j + 1]){
-                colors[j + 1] = iteratedColor;
+                colors[j + 1] = ITERATED_COLOR;
                 chart.update();
                 await sleep(currentSleepTime);
                 let temp = array[j + 1];
                 array[j + 1] = array[j];
                 array[j] = temp;
             }
-            colors[j] = defaultColor;
-            colors[j + 1] = iteratedColor;
+            colors[j] = DEFAULT_COLOR;
+            colors[j + 1] = ITERATED_COLOR;
             chart.update();
         }
     }
-    colors[0] = iteratedColor;
+    colors[0] = ITERATED_COLOR;
     chart.update();
     return array;
 }
